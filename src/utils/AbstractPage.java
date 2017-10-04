bb31e58f515705add0bf4a5268b7978da92314ba
Tim Sullivan <tsullivan13@mac.com>
2017-10-03 17:52:14 -0700

    First Commit

---
+++ src/utils/AbstractPage.java
@@ -0,0 +1,11 @@
+package utils;
+
+import io.appium.java_client.AppiumDriver;
+
+public class AbstractPage {
+	
+	//Initialize the driver with AbstractTest
+		@SuppressWarnings("rawtypes")
+		protected static AppiumDriver driver = AbstractTest.driver;
+		
+}
---
+++ src/utils/AbstractTest.java
@@ -0,0 +1,51 @@
+package utils;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import pages.SplashScreenPage;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+
+public class AbstractTest {
+	
+public static AndroidDriver<MobileElement> driver;
+	
+@BeforeSuite
+public void setup() throws MalformedURLException, InterruptedException {
+	File appDir = new File("src");
+	File app = new File(appDir, "VSCO-35(1324).apk");
+	DesiredCapabilities cap = new DesiredCapabilities();
+	cap.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+	cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Device");
+	cap.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "100");
+	cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+	cap.setCapability(AndroidMobileCapabilityType.APP_WAIT_ACTIVITY, "com.vsco.*");
+	cap.setCapability("autoGrantPermissions", "true");
+
+ try {
+	driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+} catch (MalformedURLException e) {
+	e.printStackTrace();
+} 
+driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+PageFactory.initElements(new AppiumFieldDecorator(driver), new SplashScreenPage());
+}
+
+@AfterSuite
+public void teardown() {
+	driver.quit();
+}
+}
