111a60c61bcb7ee87faaf1af1d210ba1212d80f7
Tim Sullivan <tsullivan13@mac.com>
2017-10-03 18:43:47 -0700

    adding close button object

--- src/pages/SplashScreenPage.java
+++ src/pages/SplashScreenPage.java
@@ -1,10 +1,18 @@
 package pages;
 
+import org.openqa.selenium.support.FindBy;
+
+import io.appium.java_client.MobileElement;
+
 public class SplashScreenPage {
 	//Objects
+	
+	@FindBy (id="do_it_later_button")
+	private static MobileElement closeBtn;
 	
 	
 	//Methods
 	
+
 
 }
