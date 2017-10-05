20c278111c2a02cc041f947248f22fda9900098a
Tim Sullivan <tsullivan13@mac.com>
2017-10-04 18:43:41 -0700

    adding several blank pages and adding Explore UI test

--- src/pages/ExplorePage.java
+++ src/pages/ExplorePage.java
@@ -1,5 +1,47 @@
 package pages;
 
-public class ExplorePage {
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import utils.AbstractPage;
+
+public class ExplorePage extends AbstractPage {
+	
+	//Objects
+		//'Search' button 
+		@FindBy (id="header_left_button")
+		public static MobileElement searchBtn;
+		
+		//'Settings' button 
+		@FindBy (id="com.vsco.cam:id/header_right_button")
+		public static MobileElement settingsBtn;
+		
+		//'Follow - find your friends to connect' text
+		@FindBy (id="onboarding_card_textview")
+		public static MobileElement fmfCardText;
+		
+		//Image View 
+		@FindBy (id="vsco_image_view_image")
+		public static MobileElement imageView;
+		
+		
+	//Methods
+		//Tap the Search button
+		public static void tapSearchBtn() {
+			ExplorePage.searchBtn.click();
+			PageFactory.initElements(new AppiumFieldDecorator(driver), new SearchPage());
+		}
+		
+		//Tap the Settings button
+		public static void tapSettingsBtn() {
+			ExplorePage.settingsBtn.click();
+			PageFactory.initElements(new AppiumFieldDecorator(driver), new SettingsPage());
+		}
+		
+		
+		
+		//Tap Image View (TODO)
 
 }
